f68ecc2ec90cbc2c019589c693799db8a3e71a71
JeffreyMeijer <123974@student.drenthecollege.nl>
2020-05-25 10:10:41 +0200

    Imports opnieuw toegevoegd

--- DG3-Java-4-SRC/src/com/dg3/main/models/Model.java
+++ DG3-Java-4-SRC/src/com/dg3/main/models/Model.java
@@ -1,5 +1,9 @@
 package com.dg3.main.models;
 import java.sql.*;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import com.dg3.database.DBConnection;
 public class Model {
 	Connection con = DBConnection.createConnection();
